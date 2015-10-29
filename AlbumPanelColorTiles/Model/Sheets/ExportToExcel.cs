82da75130e65fb6ec61ab23e7dec0d513b870501
Vildar Khisyametdinov <vildar82@gmail.com>
2015-10-29 14:39:32 +0300

    номера панелей по порядку в екселе

--- AlbumPanelColorTiles/Model/Sheets/ExportToExcel.cs
+++ AlbumPanelColorTiles/Model/Sheets/ExportToExcel.cs
@@ -27,19 +27,27 @@ namespace AlbumPanelColorTiles.Sheets
          worksheet.Cells[row, 1].Value = "Панели Марки АР к чертежу фасада " + album.DwgFacade;
          // Заголовки
          row++;
-         worksheet.Cells[row, 1].Value = "Марка АР";
-         worksheet.Cells[row, 2].Value = "Кол блоков";
+         worksheet.Cells[row, 1].Value = "№пп";
+         worksheet.Cells[row, 2].Value = "Марка АР";
+         worksheet.Cells[row, 3].Value = "Кол блоков";
 
          // Записываем данные
+         int i = 1;
+         int totalCountPanels = 0;
          foreach (var sheetSb in sheetsSet.SheetsMarkSB)
          {
             foreach (var sheetAr in sheetSb.SheetsMarkAR)
             {
                row++;
-               worksheet.Cells[row, 1].Value = sheetAr.MarkArFullName;//.MarkArDocumentation;
-               worksheet.Cells[row, 2].Value = sheetAr.MarkAR.Panels.Count;
+               worksheet.Cells[row, 1].Value = i++.ToString();
+               worksheet.Cells[row, 2].Value = sheetAr.MarkArFullName;
+               worksheet.Cells[row, 3].Value = sheetAr.MarkAR.Panels.Count;
+               totalCountPanels += sheetAr.MarkAR.Panels.Count;
             }
          }
+         row++;
+         worksheet.Cells[row, 2].Value = "Итого";
+         worksheet.Cells[row, 3].Value = totalCountPanels.ToString();
 
          // Список панелей по этажам
          listPanelsOnFloors(workBook, album, sheetsSet);
