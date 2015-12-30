cd62fd6698130c7cbe0bb67078352968238476e5
Vildar Khisyametdinov <vildar82@gmail.com>
2015-12-30 18:24:07 +0300

    начал CreateBtrPanelFromBase

--- AlbumPanelColorTiles/Commands.cs
+++ AlbumPanelColorTiles/Commands.cs
@@ -863,7 +863,7 @@ namespace AlbumPanelColorTiles
          Database db = doc.Database;
 
          BaseService baseService = new BaseService();
-         baseService.LoadPanels();
+         //baseService.LoadPanels();
       }      
    }
 }
