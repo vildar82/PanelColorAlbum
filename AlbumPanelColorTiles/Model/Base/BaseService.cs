7e556d67b24742cca7052ffaede1b4ea419e31e4
Vildar Khisyametdinov <vildar82@gmail.com>
2015-12-30 17:51:13 +0300

    написание теста для загрузки списка панелей ирз базы

--- AlbumPanelColorTiles/Model/Base/BaseService.cs
+++ AlbumPanelColorTiles/Model/Base/BaseService.cs
@@ -12,11 +12,45 @@ namespace AlbumPanelColorTiles.Model.Base
 {
    public class BaseService
    {
-      public void LoadPanels()
+      private Dictionary<string,Panel> _panelsFromBase;
+
+      public string XmlBasePanelsFile { get; set; }
+
+      public BaseService()
       {
-         XmlSerializer ser = new XmlSerializer(typeof(Base.Panels));         
-         string filename = Path.Combine(Commands.CurDllDir, @"Model\Base\Panels.xml");
-         Panels panels = ser.Deserialize(new FileStream(filename, FileMode.Open)) as Panels;         
+         XmlBasePanelsFile = @"c:\dev\АР\AlbumPanelColorTiles\PanelColorAlbum\AlbumPanelColorTiles\Model\Base\Panels.xml";
+      }
+      public BaseService(string xmlBasePanelsFile)
+      {
+         XmlBasePanelsFile = xmlBasePanelsFile;
+      }
+
+      public void ReadPanelsFromBase()
+      {
+         if (File.Exists(XmlBasePanelsFile))
+         {
+            throw new FileNotFoundException("XML файл базы панелей не найден.", XmlBasePanelsFile);
+         }
+
+         // TODO: Проверка валидности xml
+
+         // Чтение файла базы панелей
+         _panelsFromBase = new Dictionary<string, Panel>();
+         XmlSerializer ser = new XmlSerializer(typeof(Base.Panels));                  
+         Panels panels = ser.Deserialize(new FileStream(XmlBasePanelsFile, FileMode.Open)) as Panels;
+         var panelsList = panels.Panel.ToList();
+         foreach (var panel in panelsList)
+         {
+            try
+            {
+               _panelsFromBase.Add(panel.mark, panel);
+            }
+            catch (Exception)
+            {
+
+               throw;
+            }            
+         }
       }
    }
 }
---
+++ TessAlbumPanelAcCoreConsole/Model/Base/BaseServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using AlbumPanelColorTiles.Model.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlbumPanelColorTiles.Model.Base.Tests
+{
+   [TestFixture()]
+   public class BaseServiceTests
+   {
+      [Test()]
+      public void ReadPanelsFromXmlTest()
+      {
+         // Проверка считывания Xml базы панелей
+         BaseService baseService = new BaseService();
+         baseService.LoadPanels();         
+      }
+   }
+}
