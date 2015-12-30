c87e5b58754df89a26e0e5c10236a90206cb4da8
Vildar Khisyametdinov <vildar82@gmail.com>
2015-12-30 17:51:13 +0300

    написание теста для загрузки списка панелей ирз базы

---
+++ TessAlbumPanelAcCoreConsole/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using AlbumPanelColorTiles.Model.Select;
+using Autodesk.AutoCAD.DatabaseServices;
+using NUnit.Framework;
+
+namespace TessAlbumPanelAcCoreConsole
+{
+   [TestFixture, Apartment(ApartmentState.STA)]
+   public class Class1
+   {
+      [Test]
+      public void Test1()
+      {
+         using (var db = new Database(false, true))
+         {
+            string fileDwgTest = @"c:\temp\test\АКР\TestsAcCoreConsole\Test.dwg";
+            db.ReadDwgFile(fileDwgTest, FileOpenMode.OpenForReadAndAllShare,  false, "");
+            using (AcadLib.WorkingDatabaseSwitcher dbSwitcher = new AcadLib.WorkingDatabaseSwitcher(db))
+            {
+               SelectionBlocks select = new SelectionBlocks();
+               select.SelectSectionBlRefs();
+               Assert.AreEqual(select.SectionsBlRefs.Count, 1);
+            }
+         }
+      }
+   }
+}
---
+++ TessAlbumPanelAcCoreConsole/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("TessAlbumPanelAcCoreConsole")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TessAlbumPanelAcCoreConsole")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("5639066f-a988-4359-8d01-d88f1f8ccb05")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
