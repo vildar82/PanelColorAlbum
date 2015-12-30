65ac35118114c9bdb2c8c0cd6e76c6214b30ec2b
Vildar Khisyametdinov <vildar82@gmail.com>
2015-12-30 18:24:07 +0300

    начал CreateBtrPanelFromBase

--- TessAlbumPanelAcCoreConsole/Model/Base/BaseServiceTests.cs
+++ TessAlbumPanelAcCoreConsole/Model/Base/BaseServiceTests.cs
@@ -33,7 +33,7 @@ namespace AlbumPanelColorTiles.Model.Base.Tests
 
       [Test]
       public void ReadPanelsFromXmlTest()
-      {         
+      {
          int expectedCount = baseService.CountPanelsInBase;
          // Кол панелей в базе не должно быть равно нулю.
          Assert.AreNotEqual(expectedCount, 0);
