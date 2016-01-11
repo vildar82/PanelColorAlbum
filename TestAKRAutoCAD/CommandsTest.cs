6b481ad3a05cb35bb532d86b3e8e2fd0d0627c0f
Vildar Khisyametdinov <vildar82@gmail.com>
2016-01-11 20:09:55 +0300

    проект тестов в AutoCAD

---
+++ TestAKRAutoCAD/CommandsTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.Runtime;
+using TestAKRAutoCAD.Model.Base;
+
+[assembly: CommandClass(typeof(TestAKRAutoCAD.CommandsTest))]
+
+namespace TestAKRAutoCAD
+{
+   public class CommandsTest
+   {
+      [CommandMethod("TestCreateFacade")]
+      public void TestCreateFacade()
+      {
+         BaseServiceTests baseTest = new BaseServiceTests();
+         baseTest.CreateFacadeTest();
+      }
+   }
+}
