31e920be0e4226cbb4abb497070ee39f5dc44da1
Vildar Khisyametdinov <vildar82@gmail.com>
2016-01-12 18:12:18 +0300

    HelpCommand открытие папки с инструкциями

--- AlbumPanelColorTiles/Commands.cs
+++ AlbumPanelColorTiles/Commands.cs
@@ -328,6 +328,15 @@ namespace AlbumPanelColorTiles
          if (doc == null) return;
          Editor ed = doc.Editor;
          ed.WriteMessage("\n{0}", MsgHelp);
+         // Открытие папки с инструкциями в проводнике
+         try
+         {
+            System.Diagnostics.Process.Start("explorer", @"\\dsk2.picompany.ru\project\CAD_Settings\_Шаблоны & типовые решения\30_АР\3.01_Обучение_АКР");
+         }
+         catch (System.Exception ex)
+         {
+            Log.Error(ex, "HelpCommand");
+         }         
       }
 
       [CommandMethod("PIK", "AKR-ImagePainting", CommandFlags.Modal)]
