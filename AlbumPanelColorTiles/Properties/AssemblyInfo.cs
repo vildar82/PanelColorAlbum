a90a2ac3e2d757863e51ddfeb2bfff7a2828909b
Vildar Khisyametdinov <vildar82@gmail.com>
2016-03-17 17:50:43 +0300

    TranslatorWindows

--- AlbumPanelColorTiles/Properties/AssemblyInfo.cs
+++ AlbumPanelColorTiles/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.76.281")]
-[assembly: AssemblyFileVersion("2.0.76.281")]
+[assembly: AssemblyVersion("2.0.77.283")]
+[assembly: AssemblyFileVersion("2.0.77.283")]
