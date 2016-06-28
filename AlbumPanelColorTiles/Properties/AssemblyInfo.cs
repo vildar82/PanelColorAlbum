81090e3b6da26767a901ae3fc6a1cd8787baed00
Vildar Khisyametdinov <vildar82@gmail.com>
2016-06-28 13:25:20 +0300

    navigator

--- AlbumPanelColorTiles/Properties/AssemblyInfo.cs
+++ AlbumPanelColorTiles/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.180.409")]
-[assembly: AssemblyFileVersion("2.0.180.409")]
+[assembly: AssemblyVersion("2.0.180.410")]
+[assembly: AssemblyFileVersion("2.0.180.410")]
